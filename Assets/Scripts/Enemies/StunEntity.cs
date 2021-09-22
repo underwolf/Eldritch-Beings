0236514dc22204230e990a6b5120df0961b2b2cc
Sergioxks <59104615+Sergioxks@users.noreply.github.com>
2021-09-22 17:42:52 -0300

    finished the Stun Bell and added Astar Pathfinding to the project
    ---Changes---
    -- Added AstarPathfinding Project folder --
    	- Imported AstarPathfinding Project files
    --Scenes--
    	- Added Courtyard Scene
    --Prefabs--
    	- JersoTestingPrefabs
    		- Added CorridorApparition testing Prefab
    		- Added StunBell testing Prefab
    
    --Scripts--
    	- Added Room Elements Folder
    		- Added Courtyard Folder
    			- Added StunBell Script
    			- Added CourtyardEntity Script
    
    	- Enemies
    		- Added StunEntity Script
    
    --Images--
    	- JersoTesting
    		- Added stunBell testing sprite
    		- Added courtyardEntity testing sprite

---
+++ Assets/Scripts/Enemies/StunEntity.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunEntity : MonoBehaviour
+{
+    public void StunEnemy()
+    {
+        Debug.Log("oh noes! I has been stunned");
+    }
+}
