58874886d516d5eeef841d7ceac05fe161c9aa06
Sergioxks <59104615+Sergioxks@users.noreply.github.com>
2021-09-26 21:55:35 -0300

    Started the development of the library puzzle
    ---Changes---
    --Materials--
    	- Added Physics Materials
    		- Added Stair Physics material
    --Scenes--
    	- Added Transition Corridor - Library Scene
    	- Added Library Scene
    --Prefabs--
    	- JersoTestingPrefabs
    
    --Scripts--
    	- Room Elements
    		- Added GetDownPlatform Script
    		- Added StairsScript
    		- Added Library Folder
    			- Added LibraryDoorSeal Script
    			- Added LibraryPuzzleManager script
    
    	- Interactables
    		- Added Library Folder
    			- Added BookcaseScript
    			- Added AnimalTrophies script
    
    	- UI
    		- Effects
    			- Added MoveIn script
    --Images--
    	- JersoTesting
    		- Added animalHead1 testing image
    		- Added bookcase testing image

---
+++ Assets/Scripts/Room Elements/Library/LibraryPuzzleManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibraryPuzzleManager : MonoBehaviour
+{
+    public List<GameObject> puzzleTrophies = new List<GameObject>();
+    public int[] puzzleSequence = { 1, 2, 3, 4};
+    public bool[] puzzleStateBool = { false, false, false, false};
+    public bool puzzleState;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(puzzleTrophies.Count == 4)
+        {
+            CheckPuzzle();
+        }    
+    }
+
+    private void CheckPuzzle()
+    {
+        for(int i = 0; i <= puzzleTrophies.Count - 1; i++)
+        {
+            if(puzzleTrophies[i].GetComponent<AnimalTrophies>().puzzleId == puzzleSequence[i])
+            {
+                puzzleStateBool[i] = true;
+            }
+            else
+            {
+                puzzleStateBool[i] = false;
+            }
+        }
+
+        if(puzzleStateBool[0] && puzzleStateBool[1] && puzzleStateBool[2] && puzzleStateBool[3])
+        {
+            puzzleState = true;
+        }
+
+
+        if (puzzleState)
+        {
+            //do the thing
+        }
+        else
+        {
+            foreach(GameObject trophy in puzzleTrophies)
+            {
+                trophy.GetComponent<AnimalTrophies>().ResetPuzzle();
+            }
+
+            puzzleTrophies.Clear();
+        }
+    }
+}
