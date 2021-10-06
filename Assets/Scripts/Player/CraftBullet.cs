c393c5038fde09158f757c7f768e09a340ed44fa
underwolf <matheusozzy1@hotmail.com>
2021-10-06 20:08:29 -0300

    Revert "Fixed Commit from add fmodSupport"
    This reverts commit fe4266b50d9dcdcff034e032c8c8c37f5804644f.

--- Assets/Scripts/Player/CraftBullet.cs
+++ Assets/Scripts/Player/CraftBullet.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class CraftBullet : MonoBehaviour
 {
+
     public GameObject bullet;
     public Transform BulletPos;
     private Vector3 bulletNewPos;
@@ -19,7 +21,6 @@ public class CraftBullet : MonoBehaviour
             if(GameObject.Find("BulletOriginalPos").transform.childCount >= 10)
             {
                 GameObject.Find("CraftingCanvas").GetComponent<ProgressBar>().showWarning = true;
-                
             }
             else
             {
@@ -37,8 +38,6 @@ public class CraftBullet : MonoBehaviour
                 var bulletParent = Instantiate(bullet, bulletNewPos, Quaternion.identity);
                 bulletParent.transform.parent = BulletPos;
                 startTime = 0;
-                Debug.Log("CRAFTED BULLET");
-                FindObjectOfType<FMODFire>().FMODCRAFT();
             }
             GameObject.Find("CraftingCanvas").GetComponent<ProgressBar>().fillBar = false;
             GameObject.Find("CraftingCanvas").GetComponent<ProgressBar>().hideCraft();
@@ -55,8 +54,6 @@ public class CraftBullet : MonoBehaviour
         GameObject.Find("CraftingCanvas").GetComponent<ProgressBar>().fillBar = false;
         GameObject.Find("CraftingCanvas").GetComponent<ProgressBar>().hideCraft();
         filledBar = true;
-        Debug.Log("CRAFTED BULLET");
-        FindObjectOfType<FMODFire>().FMODCRAFT();
     }
 
 
